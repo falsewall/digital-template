03597f02824dc4e66cc0dd2f1abb0d51dd7a9254
falsewall <sawofken@gmail.com>
2015-02-04 17:52:57 -0500

    Slight change in order

--- js/main.js
+++ js/main.js
@@ -41,9 +41,9 @@ window.onload = function() {
         // Center it in X, and position its top 15 pixels from the top of the world.
         var style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
         var text = game.add.text( game.world.centerX, 15, "Build something Ketchup.", style );
-
+	text.anchor.setTo( 0.5, 0.3 );
 		bouncy.events.onInputDown.add(listener, this);
-        text.anchor.setTo( 0.5, 0.3 );
+        
     }
     function listener() {
 			counter++;
