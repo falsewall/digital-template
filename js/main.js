bb5c3efb39a3da2f73f43d3e2bcf6fe4e4947bd9
falsewall <sawofken@gmail.com>
2015-02-05 02:49:59 -0500

    walljump seperate function

--- js/main.js
+++ js/main.js
@@ -23,6 +23,7 @@ var tileset;
 var layer;
 var p;
 var cursors;
+var touched=0;
 
 function preload() {
 	game.load.tilemap('map', 'assets/tilesheets/industrial.json', null, Phaser.Tilemap.TILED_JSON);
@@ -60,10 +61,46 @@ function create() {
 
 
 }
+function doublecheck(){//run every time player hits a wall. sets touched and leaves it till another wall is hit or ground is landed
+   if (cursors.up.isDown && !p.body.blocked.down ) {
+	   
+	   	if (touched >== 0) //right touched or nothing touched
+	{
+		if(p.body.blocked.left)
+		{
+			touched=-1;//just hit left wall
+		}
+	}
+	if(touched <== 0)//left touched
+	{
+		if(p.body.blocked.right)
+		{
+			touched=1;//just hit right wall
+		}
+	}
+	if(p.body.blocked.down)
+	{
+		touched=0;//reset touched on landing
+	}
+	
+
+      if (cursors.left.isDown && p.body.blocked.left && touched >==0 ) {
+        p.body.velocity.x = 450;
+        p.body.velocity.y = -400;
+      }
+	   if (cursors.right.isDown && p.body.blocked.right && touched <== 0) {
+		p.body.velocity.x = -450;
+		p.body.velocity.y = -400;
+      }
+    }
+
+
+}
 
 function update() {
 
     game.physics.arcade.collide(p, layer);
+	doublecheck();//should deal with walljumping
 	if (p.body.blocked.down)
 	{
 	    if(p.body.velocity.x < 0)
@@ -77,16 +114,7 @@ function update() {
 
 	
 	}
-   if (cursors.up.isDown && !p.body.touching.down ) {
-      if (cursors.left.isDown && p.body.touching.left) {
-        p.body.velocity.x = 450;
-        p.body.velocity.y = -400;
-      }
-	   if (cursors.right.isDown && p.body.touching.right) {
-		p.body.velocity.x = -450;
-		p.body.velocity.y = -400;
-      }
-    }
+
 
     if (cursors.up.isDown)
     {
