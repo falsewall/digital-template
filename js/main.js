27e4468ca90215ffef7933e0bd4a85323fbc3c04
falsewall <sawofken@gmail.com>
2015-02-04 19:24:10 -0500

    Lets see what not trimming does

--- js/main.js
+++ js/main.js
@@ -48,7 +48,7 @@ var botData = {
     "filename": "running bot.swf/0000",
     "frame": { "x": 34, "y": 128, "w": 56, "h": 60 },
     "rotated": false,
-    "trimmed": true,
+    "trimmed": false,
     "spriteSourceSize": { "x": 0, "y": 2, "w": 56, "h": 60 },
     "sourceSize": { "w": 56, "h": 64 }
 },
@@ -56,7 +56,7 @@ var botData = {
     "filename": "running bot.swf/0001",
     "frame": { "x": 54, "y": 0, "w": 56, "h": 58 },
     "rotated": false,
-    "trimmed": true,
+    "trimmed": false,
     "spriteSourceSize": { "x": 0, "y": 3, "w": 56, "h": 58 },
     "sourceSize": { "w": 56, "h": 64 }
 },
@@ -64,7 +64,7 @@ var botData = {
     "filename": "running bot.swf/0002",
     "frame": { "x": 54, "y": 58, "w": 56, "h": 58 },
     "rotated": false,
-    "trimmed": true,
+    "trimmed": false,
     "spriteSourceSize": { "x": 0, "y": 3, "w": 56, "h": 58 },
     "sourceSize": { "w": 56, "h": 64 }
 },
