eca06c114680e1e3f4bf20c1eed39fb04ca81b71
falsewall <sawofken@gmail.com>
2015-02-26 12:20:45 -0500

    moveout

--- js/main.js
+++ js/main.js
@@ -43,8 +43,7 @@ function spawnGui()
 		button_b= game.add.sprite(200, 500, 'Buttons');
 		button_b.frame=5;
 		button_b.inputEnabled = true;
-		this.text =game.add.text(0, 200, '100hp');
-		this.hud = game.add.text(0,20,'IntervieweR HP: '+100);
+
 		
 		button_a.events.onInputDown.add(listener, this);
 		button_b.event.onInputDown.add(listener2, this);
@@ -100,12 +99,9 @@ function listener2(){
 }
 function create() {
 
-
-
-	
-
 	game.stage.backgroundColor = '#FFCCFF';
-	
+	text =game.add.text(0, 200, '100hp');
+	hud = game.add.text(0,20,'IntervieweR HP: '+100);
 
 	
 
