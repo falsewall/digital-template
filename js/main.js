c138a40b7fcf71c3ec1c42ba349706be65f0ce84
falsewall <sawofken@gmail.com>
2015-02-26 09:15:34 -0500

    doop

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ window.onload = function() {
     
 
 var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update, render: render });
-
+var hud;
 
 
 
