3bffb9690cb3eafc98295de1cac63a4fb2958fba
falsewall <sawofken@gmail.com>
2015-03-19 11:59:15 -0400

    er, issues much?

--- js/main.js
+++ js/main.js
@@ -1,278 +1,252 @@
-window.onload = function() {
-    // You might want to start with a template that uses GameStates:
-    //     https://github.com/photonstorm/phaser/tree/master/resources/Project%20Templates/Basic
-    
-    // You can copy-and-paste the code from any of the examples at http://examples.phaser.io here.
-    // You will need to change the fourth parameter to "new Phaser.Game()" from
-    // 'phaser-example' to 'game', which is the id of the HTML element where we
-    // want the game to go.
-    // The assets (and code) can be found at: https://github.com/photonstorm/phaser/tree/master/examples/assets
-    // You will need to change the paths you pass to "game.load.image()" or any other
-    // loading functions to reflect where you are putting the assets.
-    // All loading functions will typically all be found inside "preload()".
-    
-    "use strict";
-    
-
-var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update, render: render });
-
-
-var grabbed = 40;
-var map;
-var tileset;
-var layer;
-var p;
-var cursors;
-var touched=0;
-var jump;
-var jump2;
-var jump3;
-var coinNoise;
-var music;
-var heart;
-var bmd;
-var group;
-var hud;
-var emitter;
-var winnerText;
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update: update });
 
 function preload() {
-	game.load.tilemap('map', 'assets/tilesheets/industrial.json', null, Phaser.Tilemap.TILED_JSON);
-    game.load.image('tileset', 'assets/tilesheets/Industrial-TileSheet.png');
-	game.load.image('player', 'assets/sprites/phaser-dude.png');
-	game.load.audio('dog1', 'assets/sounds/effects/jump_01.ogg');
-	game.load.audio('dog2', 'assets/sounds/effects/jump_02.ogg');
-	game.load.audio('dog3', 'assets/sounds/effects/jump_03.ogg');
-	game.load.audio('coin', 'assets/sounds/effects/coin.ogg');
-	game.load.audio('type1', 'assets/sounds/effects/type1.ogg');
-	game.load.spritesheet('heart_beat', 'assets/sprites/heart_beat_32x32.png', 32, 32);
-	game.load.spritesheet('panda', 'assets/sprites/Panda.png', 32, 32);
-	game.load.image('pandicle', 'assets/sprites/pandaParticle.png');
 
-	
-
+    game.load.atlas('breakout', 'assets/sprites/breakout.png', 'assets/sprites/breakout.json');
+    game.load.image('cats', 'assets/cats.png');
+	game.load.spritesheet('phone', 'assets/sprites/phone_244x134.png', 122, 134, 2);
 
 }
+
+var ball;
+var paddle;
+var bricks;
+
+var ballOnPaddle = true;
+
+var lives = 3;
+var score = 0;
+
+var scoreText;
+var livesText;
+var introText;
+var phone;
+var phonesLeft;
+var phoneList;
+var timer;
+
+var s;
 
 function create() {
 
-	jump = game.add.audio('dog1');
-	jump2 = game.add.audio('dog2');
-	jump3 = game.add.audio('dog3');
-	coinNoise = game.add.audio('coin');
-	music = game.add.audio('type1', 1 , true);
-	music.play('', 0, 1, true);
-	
-
-	
-
 
 
 	
-	game.physics.startSystem(Phaser.Physics.ARCADE);
-	game.stage.backgroundColor = '#154632';
-	
-	map = game.add.tilemap('map');
-	map.addTilesetImage('Industrial-TileSheet', 'tileset');
-	map.setCollisionBetween(0, 2);// 104 66 99 24 10 0 1
-	map.setCollisionBetween(104, 105);
-	map.setCollisionBetween(66, 67);
-	map.setCollisionBetween(99, 100);
-	map.setCollisionBetween(24, 25);
-	map.setCollisionBetween(10, 11);
-	
-	
-	layer = map.createLayer('Tile Layer 1');
-	layer.resizeWorld();
-	//layer.debug = true;
-
-	p= game.add.sprite(500, 500, 'panda');
-	p.animations.add('stand', [0, 1], 10, true);
-	p.animations.add('jump', [5, 6, 7], 10, true);
-	p.animations.add('left', [25, 26, 27, 31, 32], 10, true);
-    p.animations.add('right', [15, 16, 17, 20, 21], 10, true);
 
 	
-	game.physics.enable(p);
+	timer = game.time.now + 100;
+    game.physics.startSystem(Phaser.Physics.ARCADE);
 
-    game.physics.arcade.gravity.y = 500;
+    //  We check bounds collisions against all walls other than the bottom one
+    game.physics.arcade.checkCollision.down = false;
 
-    p.body.bounce.y = 0.1;
-    p.body.linearDamping = 1;
-    p.body.collideWorldBounds = true;
+    s = game.add.tileSprite(0, 0, 800, 600, 'cats');
 
-    game.camera.follow(p);
+    bricks = game.add.group();
+    bricks.enableBody = true;
+    bricks.physicsBodyType = Phaser.Physics.ARCADE;
 
-    cursors = game.input.keyboard.createCursorKeys();
-//////////////////////////////////////
-	heartsetup();
+    var brick;
 
-	
-	hud = game.add.text(0, 0, 'Hearts remaining: '+grabbed);
-	winnerText = game.add.text(0, 0, "", {
-        font: "69px Arial",
-        fill: "#ff0040",
-        align: "center"
-    });
-	winnerText.fixedToCamera = true;
-    hud.fixedToCamera= true;
-	
-	emitter = game.add.emitter(0, 0, 1000);
-	emitter.makeParticles('pandicle');
-	p.addChild(emitter);
-	
-	emitter.lifespan = 500;
-	emitter.maxParticleSpeed = new Phaser.Point(-100,50);
-	emitter.minParticleSpeed = new Phaser.Point(-200,-50);
-
-
-
-}
-
-function heartsetup(){
-	
-	heart = game.add.sprite(90, 2500, 'heart_beat');
-	heart.animations.add('beat');
-	heart.play('beat', 5, true);
-	
-	    group = game.add.group();
-		group.enableBody = true;
-		group.physicsBodyType = Phaser.Physics.ARCADE;
-
-    //  Add a bunch of sprites in random positions to the group
-    for (var i = 0; i < 40; i++)
+    for (var y = 0; y < 4; y++)
     {
-        var c = group.create(game.world.randomX, game.world.randomY, 'heart_beat');
-		c.name = 'heart' + i;
-		c.body.immovable = true;
-    }
-	group.callAll('animations.add', 'animations', 'beatit',[0,1,2,3] , 5, true);
-	group.callAll('play', null, 'beatit');
-    
-
-}
-function collisionHandler (player, pickup) {
-    //  If the player collides with the chillis then they get eaten :)
-    //  The chilli frame ID is 17
-	grabbed--;
-	hud.setText( 'Hearts Remaining: '+grabbed);
-	coinNoise.play();
-    pickup.kill();
-}
-
-function doublecheck(){//run every time player hits a wall. sets touched and leaves it till another wall is hit or ground is landed
-game.physics.arcade.collide(p, layer);
-   if (cursors.up.isDown && !p.body.blocked.down ) {
-	   
-		if (cursors.left.isDown && p.body.blocked.left && touched > -1 ) {
-			jump2.play();
-			p.body.velocity.x = 500;
-			p.body.velocity.y = -500;
-		}
-		if (cursors.right.isDown && p.body.blocked.right && touched < 1) {
-			jump3.play();
-			p.body.velocity.x = -500;
-			p.body.velocity.y = -500;
-		}
-	   
-	   	if (touched > -1) //right touched or nothing touched
-		{
-			if(p.body.blocked.left)
-			{
-				touched=-1;//just hit left wall
-			}
-		}
-		if(touched < 1)//left touched
-		{
-			if(p.body.blocked.right)
-			{
-				touched=1;//just hit right wall
-			}
-		}
-	
-    }
-	if(p.body.blocked.down)
-	{
-		touched=0;//reset touched on landing
-	}
-
-
-}
-
-function update() {
-
-    //game.physics.arcade.collide(p, layer);
-	game.physics.arcade.collide(p, group, collisionHandler, null, this);
-    game.physics.arcade.collide(group, layer);
-	doublecheck();//should deal with walljumping
-	if (p.body.blocked.down)
-	{
-	    if(p.body.velocity.x < 0)
-		{
-			p.body.velocity.x+=100;
-		}
-		if(p.body.velocity.x > 0)
-		{
-			p.body.velocity.x-=100;
-		}
-		if(p.body.velocity.x === 0)
-		{
-			p.animations.play('standing', 5, true);
-		}
-
-	
-	}
-	else 
-	{
-		if(p.body.velocity.x < 0)
-		{
-			p.body.velocity.x+=50;
-		}
-		if(p.body.velocity.x > 0)
-		{
-			p.body.velocity.x-=50;
-		}
-		if(p.body.velocity.x === 0)
-		{
-			p.animations.play('standing', 5, true);
-		}
-	}
-
-
-    if (cursors.up.isDown)
-    {
-        if (p.body.onFloor())
+        for (var x = 0; x < 15; x++)
         {
-			p.animations.play('jump', 10, true);
-			jump.play();
-            p.body.velocity.y = -500;
+            brick = bricks.create(120 + (x * 36), 100 + (y * 52), 'breakout', 'brick_' + (y+1) + '_1.png');
+            brick.body.bounce.set(1);
+            brick.body.immovable = true;
         }
     }
 
-    if (cursors.left.isDown)
+    paddle = game.add.sprite(game.world.centerX, 500, 'breakout', 'paddle_big.png');
+    paddle.anchor.setTo(0.5, 0.5);
+
+    game.physics.enable(paddle, Phaser.Physics.ARCADE);
+
+    paddle.body.collideWorldBounds = true;
+    paddle.body.bounce.set(1);
+    paddle.body.immovable = true;
+
+    ball = game.add.sprite(game.world.centerX, paddle.y - 16, 'breakout', 'ball_1.png');
+    ball.anchor.set(0.5);
+    ball.checkWorldBounds = true;
+
+    game.physics.enable(ball, Phaser.Physics.ARCADE);
+
+    ball.body.collideWorldBounds = true;
+    ball.body.bounce.set(1);
+
+    ball.animations.add('spin', [ 'ball_1.png', 'ball_2.png', 'ball_3.png', 'ball_4.png', 'ball_5.png' ], 50, true, false);
+
+    ball.events.onOutOfBounds.add(ballLost, this);
+
+    scoreText = game.add.text(32, 550, 'score: 0', { font: "20px Arial", fill: "#ffffff", align: "left" });
+    livesText = game.add.text(680, 550, 'lives: 3', { font: "20px Arial", fill: "#ffffff", align: "left" });
+    introText = game.add.text(game.world.centerX, 400, '- click to start -', { font: "40px Arial", fill: "#ffffff", align: "center" });
+    introText.anchor.setTo(0.5, 0.5);
+
+    game.input.onDown.add(releaseBall, this);
+
+	   var mx = game.width - 122;
+    var my = game.height - 133;
+
+    for (var i = 0; i < 5; i++)
     {
-        p.body.velocity.x = -500;
-		p.animations.play('left', 15, true);
+        var sprite = game.add.sprite(game.rnd.integerInRange(0, mx), game.rnd.integerInRange(0, my), 'phone');
+
+        sprite.inputEnabled = true;
+
+        sprite.input.useHandCursor = true;
+		sprite.ringing=false;
+		sprite.online=true;
+		phonesLeft++;
+		sprite.animations.add('ring');///////////////
+		sprite.animations.play('ring', 6, true);
+		if(i===3){		sprite.animations.stop(null,true);}
+
+
+
+
+        sprite.events.onInputDown.add(destroySprite, this);
     }
-    else if (cursors.right.isDown)
-    {
-		p.animations.play('right', 15, true);
-        p.body.velocity.x = 500;
-		
-    }
-	if(grabbed=== 0)
+}
+	function destroySprite (sprite) {
+	if(sprite.ringing===true)
 	{
-		emitter.emitParticle();
-		winnerText.setText("The pandas live on!");
+		phonesLeft--;
+		sprite.destroy();
 	}
 }
+function tryToRing()
+{
+	
+}
+function update () {
 
-function render() {
+    //  Fun, but a little sea-sick inducing :) Uncomment if you like!
+    // s.tilePosition.x += (game.input.speed.x / 2);
 
-    //game.debug.body(p);
-    //game.debug.bodyInfo(p, 32, 320);
+    paddle.x = game.input.x;
+
+    if (paddle.x < 24)
+    {
+        paddle.x = 24;
+    }
+    else if (paddle.x > game.width - 24)
+    {
+        paddle.x = game.width - 24;
+    }
+
+    if (ballOnPaddle)
+    {
+        ball.body.x = paddle.x;
+    }
+    else
+    {
+        game.physics.arcade.collide(ball, paddle, ballHitPaddle, null, this);
+        game.physics.arcade.collide(ball, bricks, ballHitBrick, null, this);
+    }
 
 }
 
+function releaseBall () {
 
-};
+    if (ballOnPaddle)
+    {
+        ballOnPaddle = false;
+        ball.body.velocity.y = -300;
+        ball.body.velocity.x = -75;
+        ball.animations.play('spin');
+        introText.visible = false;
+    }
+
+}
+
+function ballLost () {
+
+    lives--;
+    livesText.text = 'lives: ' + lives;
+
+    if (lives === 0)
+    {
+        gameOver();
+    }
+    else
+    {
+        ballOnPaddle = true;
+
+        ball.reset(paddle.body.x + 16, paddle.y - 16);
+        
+        ball.animations.stop();
+    }
+
+}
+
+function gameOver () {
+
+    ball.body.velocity.setTo(0, 0);
+    
+    introText.text = 'Game Over!';
+    introText.visible = true;
+
+}
+function gameWon (){
+	ball.body.velocity.setTo(0,0);
+	introText.text = 'You found your phone!';
+	introText.visable = true;
+	
+}
+
+function ballHitBrick (_ball, _brick) {
+
+    _brick.kill();
+
+    score += 10;
+
+    scoreText.text = 'score: ' + score;
+
+    //  Are they any bricks left?
+    if (bricks.countLiving() == 0)
+    {
+        //  New level starts
+        score += 1000;
+        scoreText.text = 'score: ' + score;
+        introText.text = '- Next Level -';
+
+        //  Let's move the ball back to the paddle
+        ballOnPaddle = true;
+        ball.body.velocity.set(0);
+        ball.x = paddle.x + 16;
+        ball.y = paddle.y - 16;
+        ball.animations.stop();
+
+        //  And bring the bricks back from the dead :)
+        bricks.callAll('revive');
+    }
+
+}
+
+function ballHitPaddle (_ball, _paddle) {
+
+    var diff = 0;
+
+    if (_ball.x < _paddle.x)
+    {
+        //  Ball is on the left-hand side of the paddle
+        diff = _paddle.x - _ball.x;
+        _ball.body.velocity.x = (-10 * diff);
+    }
+    else if (_ball.x > _paddle.x)
+    {
+        //  Ball is on the right-hand side of the paddle
+        diff = _ball.x -_paddle.x;
+        _ball.body.velocity.x = (10 * diff);
+    }
+    else
+    {
+        //  Ball is perfectly in the middle
+        //  Add a little random X to stop it bouncing straight up!
+        _ball.body.velocity.x = 2 + Math.random() * 8;
+    }
+
+}
